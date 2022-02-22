bd1bc2db7462e863c8e1a40b9e594072ac5cda01
CKMolinga <ck.molinga@gmail.com>
2022-02-22 14:11:33 +0100

    fetched weather api call from Accuweather api

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,5 +1,17 @@
  const key = "oVftZYsEptWDYDsruNbvlMnEsFThbE6h";
 
+ //  get weather information 
+ const getWeather = async(id) => {
+     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+     const query = `${id}?apikey=${key}`;
+
+     const response = await fetch(base + query);
+     const data = await response.json();
+
+     return data[0];
+ };
+
+ //  get city information 
  const getCity = async(city) => {
 
      const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -9,8 +21,11 @@
      const data = await response.json();
 
      return data[0];
- }
 
- getCity('manchester')
-     .then(data => console.log(data))
-     .catch(err => console.log(err));
+ };
+
+ getCity('manchester').then(data => {
+     return getWeather(data.Key);
+ }).then(data => {
+     console.log(data);
+ }).catch(err => console.log(err));
