f167f52c9d64af81fff933a635fa4ae5fc5a90da
CKMolinga <ck.molinga@gmail.com>
2022-02-24 09:31:57 +0100

    updated UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,46 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+    // remove the d-none class if present
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+};
 
 const updateCity = async(city) => {
 
     const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.key);
+    const weather = await getWeather(cityDets.Key);
+    return { cityDets, weather };
 
-    return {
-        // cityDets: cityDets,
-        // weather: weather
-
-        // object shorthand 
-        // ==> We can do this when the key and value and thesame 
-
-        cityDets,
-        weather
-    };
-
-}
+};
 
 cityForm.addEventListener('submit', e => {
+    // prevent default action
     e.preventDefault();
 
-    // Get city value 
+    // get city value
     const city = cityForm.city.value.trim();
     cityForm.reset();
 
-    // Update UI with New City 
+    // update the ui with new city
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err));
-})
+});
