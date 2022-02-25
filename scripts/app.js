3233ef4a963024ce56ca4488bb221772f5033989
CKMolinga <ck.molinga@gmail.com>
2022-02-26 00:35:20 +0100

    Added Update image and icon functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
 
@@ -16,6 +18,13 @@ const updateUI = (data) => {
       <span>&deg;C</span>
     </div>
   `;
+
+    // Update the night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc);
 
     // remove the d-none class if present
     if (card.classList.contains('d-none')) {
